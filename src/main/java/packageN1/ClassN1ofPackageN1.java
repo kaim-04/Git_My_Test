c89b760c5f89295b2f74c15b583f1b7d4faae474
Rustam <kaim010203@gmail.com>
2023-03-16 09:12:43 +0300

    Adding new class to package packageN1

---
+++ src/main/java/packageN1/ClassN1ofPackageN1.java
@@ -0,0 +1,30 @@
+package packageN1;
+
+public class ClassN1ofPackageN1 {
+
+
+    long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
